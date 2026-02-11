9e36c4b18ac8d1ba0b59670af2a93f902131897d
pgrewal33 <pg10@kent.ac.uk>
2026-02-11 10:06:52 +0000

    working

--- part_3.py
+++ part_3.py
@@ -1,22 +1,31 @@
 def motor_controller():
-    try:
-        motor_speed = int(input("enter motor speed: "))
-        print (f"set speed to {motor_speed}")
+    while True:    
+        try:
+            motor_speed = int(input("enter motor speed: "))
+            if motor_speed >= 100:
+                print("out of range")
+            elif motor_speed <= 0:
+                print("out of range")
+            else:
+                print (f"set speed to {motor_speed}")
+                break
 
-    except ValueError:
-        print ("Error: Corrupted Signal. Maintaining current speed.")
+        except ValueError:
+            print ("Error: Corrupted Signal. Maintaining current speed.")
 
 motor_controller()
 
 def get_coordinates():
     while True:
 
-        coordinate = input("enter x coordinate: ")
+        coordinate =  input("enter x coordinate: ")
     
         if not coordinate.isdigit():
             print ("invalid coordinate")
-            
-        
+        elif coordinate.isdigit() >= 100:
+            print("out of range")
+        elif coordinate.isdigit() <= -100:
+            print("out of range")
         else:
             break
     
