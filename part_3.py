414280c8190b5ab7a7f682f0e15bb9e01ea6eed5
pgrewal33 <pg10@kent.ac.uk>
2026-02-11 09:51:45 +0000

    working

--- part_3.py
+++ part_3.py
@@ -1,1 +1,24 @@
+def motor_controller():
+    try:
+        motor_speed = int(input("enter motor speed: "))
+        print (f"set speed to {motor_speed}")
+
+    except ValueError:
+        print ("Error: Corrupted Signal. Maintaining current speed.")
+
+motor_controller()
+
+def get_coordinates():
+    while True:
+
+        coordinate = input("enter x coordinate: ")
+    
+        if not coordinate.isdigit():
+            print ("invalid coordinate")
+            
+        
+        else:
+            break
+    
+get_coordinates()
 
